f049def1402c7ce91fe9b53780d8b83ecd1486eb
Zack <thurston@cwolf.cs.sonoma.edu>
2014-10-23 18:54:16 -0700

    changed and compiled hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 using namespace std;
-
+//y
 int main(int argc, char* argv[])
 {
-  cout << "Hello, " << ARGV[10] << "!" << endl;
+  cout << "Hello, " << argv[10] << "!" << endl;
   return 0;
 }
